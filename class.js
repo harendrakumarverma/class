b6524daddce21d41f0bd1720dfd861a773720539
netstorm <netstorm@info@cavisson.com>
2017-09-25 13:11:49 +0530

    1st comit

---
+++ class.js
@@ -0,0 +1,30 @@
+/**
+ * Classes and Inheritance
+ * Code Example from http://www.es6fiddle.net/
+ */
+
+class Rectangle{
+	constructor(length,breadth){
+		this.name='rectangle';
+		this.length=length;
+		this.breadth=breadth;
+	}
+	sayName(){
+		console.log('Name is : ',this.name,'Area is :',this.length*this.breadth)
+	}
+}
+class Square extends Rectangle{
+	constructor(length=10){
+		super(length,length)
+                this.name='square';
+        }
+	get area(){
+		console.log('Name is : ',this.name,'Area is :',this.length*this.breadth)
+	}
+
+}
+
+var sq = new Square(5);
+sq.sayName();
+new Square().area;
+
